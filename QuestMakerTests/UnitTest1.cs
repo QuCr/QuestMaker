34b008a180b8322d48e179a4b810a06f87cca45d
Croes Quinten <66731752+QuCr@users.noreply.github.com>
2020-10-03 00:55:47 +0200

    Added Test project

---
+++ QuestMakerTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace QuestMakerTests {
+    [TestClass]
+    public class UnitTest1 {
+        [TestInitialize]
+        public void Init() {
+            Console.WriteLine("Hello world");
+        }
+
+        [TestMethod]
+        public void TestMethod1() {
+        }
+    }
+}
